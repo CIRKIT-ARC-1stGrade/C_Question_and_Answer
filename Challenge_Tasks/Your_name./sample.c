0973f0583eefaeaa1c8cf308cee72b757f8823a2
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-02-26 05:39:22 +0900

    Refactor c code and Fix error
    -  char myname[12] = {"TANAKA DAIKI"};
      wrong number "12". You forget '\0' charactor!
    
    -  char array[26] = {"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
      ditto.

--- Challenge_Tasks/Your_name./sample.c
+++ Challenge_Tasks/Your_name./sample.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
 int main(void){
-
-  char myname[12] = {"TANAKA DAIKI"};
+  const char array[] = {"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
   int i, j;
-  char array[26] = {"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
 
-  printf("%s\n", myname);
+  char myname[] = {"TANAKA DAIKI"};
+  puts(myname);
   
-  for (i = 0; array[i] != '\0'; i++){
-    for (j = 0; myname[j] != '\0'; j++){
-      if (array[i] == myname[j]) printf("%c", array[i]);
+  for (i = 0; array[i] != '\0'; i++) { // A to Z
+    for (j = 0; myname[j] != '\0'; j++) { // On your name
+      if (array[i] == myname[j]) putchar(array[i]); // display the found alfabet
     }
   }
   putchar('\n');
