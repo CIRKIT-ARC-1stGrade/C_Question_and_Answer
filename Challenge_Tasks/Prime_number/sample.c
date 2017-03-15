848b0f9b2a117fa367f844750ff1fef2fc12f7d7
tanacchi <q111026d@mail.kyutech.jp>
2017-03-15 22:12:19 +0900

    Add sample code (prime number)

---
+++ Challenge_Tasks/Prime_number/sample.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void show_help(const char* prog_name) {
+  printf("Usage:%s [number]\n"
+         "[number] is max number of display numbers\n", prog_name);
+}
+
+int is_Prime(int subject) {
+  int i;
+  for (i = 2; i <= (subject / 2); i++)
+    if (!(subject % 2)) return 0;
+  return 1;
+}
+
+int main(int argc, char** argv) {
+  
+  if (argc < 2) {
+    show_help(*argv /* argv[0] */);
+    return 0;
+  }
+
+  const int MAX = atoi(argv[1]);
+
+  int subject;
+  for (subject = 2; subject <= MAX; subject++)
+    if (is_Prime(subject)) printf("%d\n", subject);
+    
+  return 0;
+}
